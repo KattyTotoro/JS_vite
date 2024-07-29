d4f265063396c781c5d6f4d702360a3cbdaf12a5
KattyTotoro <89184036854@yandex.ru>
2024-07-29 22:02:42 +0300

    gfhfh

--- src/main.ts
+++ src/main.ts
@@ -3831,11 +3831,69 @@ import './style.scss'
 // ___________________________________________________________________________________________________________
 
 
+// Задачи из учебника
+
+// const user = {
+
+// } as Record<string, any> 
+
+// user.name = 'John'
+// user['surname'] = 'Smith'
+// console.log(user)
+// user['name'] = 'Pete'
+// console.log(user)
+// delete user.name
+// console.log(user)
+
+// let schedule = {};
+
+// function isEmpty(obj:object) {
+//       for(let key in obj) {           
+//             return false
+//       }
+//       return true
+// }
+
+// console.log(isEmpty(schedule))
+
+// schedule["8:30"] = "get up"
+
+// console.log(isEmpty(schedule))
 
 
+// let salaries = {
+//       John: 100,
+//       Ann: 160,
+//       Pete: 130
+//     }
+
+//     let sum = 0
+
+//     for(let key in salaries) {
+//       sum += salaries[key]
+//     }
+
+//     console.log(sum)
 
 
+//     let menu = {
+//       width: 200,
+//       height: 300,
+//       title: "My menu"
+//     }
 
+//     console.log(menu)
+
+//     function multiplyNumeric(obj:object) {
+//       for(let key in obj) {
+//             if(typeof(obj[key]) == 'number')
+//                   obj[key] *= 2
+//       }
+//     }
+
+//     multiplyNumeric(menu)
+
+//     console.log(menu)
 
 
 // *********************************************************************************************************
